3c92694c2489237ef35aba9a0bee2e4657d671c3
Tyurinam <tyurinaml@gmail.com>
2017-04-06 05:37:23 +0300

    Add files via upload

---
+++ OpenCartLocalMarina/admin/language/en-gb/exposition/module/trois_artistes.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']    = 'Trois_artistes';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified Trois_artistes module!';
+$_['text_edit']        = 'Edit Trois_artistes Module';
+
+// Entry
+$_['entry_name']       = 'Module Name';
+$_['entry_limit']      = 'Limit';
+$_['entry_width']      = 'Width';
+$_['entry_height']     = 'Height';
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Trois_artistes module!';
+$_['error_name']       = 'Module Name must be between 3 and 64 characters!';
+$_['error_width']      = 'Width required!';
+$_['error_height']     = 'Height required!';
