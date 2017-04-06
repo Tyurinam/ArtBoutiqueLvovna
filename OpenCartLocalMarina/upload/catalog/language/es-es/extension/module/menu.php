3cf7adbd1e37267d3e170885b397bc070aae1630
Tyurinam <tyurinaml@gmail.com>
2017-04-06 06:54:48 +0300

    Add files via upload

---
+++ OpenCartLocalMarina/upload/catalog/language/es-es/extension/module/menu.php
@@ -0,0 +1,14 @@
+<?php
+// Heading
+$_['heading_title'] = 'MenuModule';
+
+// Text
+$_['text_tax']      = 'Sin Iva:';
+//*********************************************************************************MenuModule Marina 
+$_['text_menumodule_1'] = 'Exposición';
+$_['text_menumodule_2'] = 'Tienda';
+$_['text_menumodule_3'] = 'Artistas';
+$_['text_menumodule_4'] = 'Noticias';
+$_['text_menumodule_5'] = 'Videos';
+$_['text_menumodule_6'] = 'Contactos';
+//*********************************************************************************MenuModule Marina 
---
+++ OpenCartLocalMarina/upload/catalog/language/es-es/extension/module/trois_artistes.php
@@ -0,0 +1,6 @@
+<?php
+// Heading
+$_['heading_title'] = 'Tres artistas';
+
+// Text
+$_['text_tax']      = 'Sin Iva:';
